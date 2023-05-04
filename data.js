8f57543033c734a15f914241162ba8b5e3569dfc
Prathamesh Mhetre <87199743+prathamesh2503@users.noreply.github.com>
2023-05-04 14:57:33 +0530

    final

---
+++ data.js
@@ -0,0 +1,66 @@
+let shopItemsData = [{
+
+    id:"p1",
+    name:"Bat",
+    price:100,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P1.png"
+},
+{   
+    id:"p2",
+    name:"Ball",
+    price:200,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P2.png"
+}, 
+{   
+    id:"p3",
+    name:"Badminton",
+    price:300,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P3.png"
+}, 
+{   
+    id:"p4",
+    name:"Racket",
+    price:400,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P4.png"
+},
+{   
+    id:"p5",
+    name:"Hockey",
+    price:800,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P5.png"
+},
+
+{   
+    id:"p6",
+    name:"Football",
+    price:600,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P6.png"
+},
+
+{   
+    id:"p7",
+    name:"Helmet",
+    price:500,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P7.png"
+},
+
+
+{   
+    id:"p8",
+    name:"Hand Gloves",
+    price:1000,
+    desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/P9.png"
+},
+
+
+
+
+]; // array of objects
